9befb4731d17f76abcd50090272bcdfd28ae7c6a
alejandrorjh <alejandro.juarez@cetys.edu.mx>
2016-09-05 19:34:07 -0700

    Created blank pages for each view by adding angular routes.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phylogeneticTreesApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the phylogeneticTreesApp
+ */
+angular.module('phylogeneticTreesApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/treegeneration.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name phylogeneticTreesApp.controller:TreegenerationCtrl
+ * @description
+ * # TreegenerationCtrl
+ * Controller of the phylogeneticTreesApp
+ */
+angular.module('phylogeneticTreesApp')
+  .controller('TreegenerationCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
